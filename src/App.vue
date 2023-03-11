502fb15c800f0ef9da4c4fa936aa9ff1bf1b0f8d
bri <bcamesi@gmail.com>
2023-03-11 17:54:05 -0500

    changing props

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,12 @@
       <friend-contact 
       name="Manuel Lorenz" 
       phone-number="0123 45678 90" email="manuel@localhost.com"
+      is-favorite="1"
       ></friend-contact>
       <friend-contact 
       name="Julie Jones" 
       phone-number="0987 654421 21" email="julie@localhost.com"
+      is-favorite="0"
       ></friend-contact>
     </ul>
   </section>
